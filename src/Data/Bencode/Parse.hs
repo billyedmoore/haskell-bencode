616aa66b4092af570ada1662803b0ef83250d5ce
Billy Moore <billyedmoore@yahoo.com>
2025-12-28 21:11:53 +0000

    Add parseStr to parse strings and associated tests.

--- src/Data/Bencode/Parse.hs
+++ src/Data/Bencode/Parse.hs
@@ -1,6 +1,7 @@
-module Data.Bencode.Parse (parse, parseInt, BencodeValue (..)) where
+module Data.Bencode.Parse (parse, parseInt, parseStr, BencodeValue (..)) where
 
 import Data.Bifunctor (first)
+import Data.Char (isNumber)
 import Text.Read (readMaybe)
 
 data BencodeValue
@@ -9,6 +10,14 @@ data BencodeValue
   | BencodeList [BencodeValue]
   | BencodeMap [(String, BencodeValue)]
   deriving (Eq, Show)
+
+-- Return "" if s has leading zero otherwise return s
+checkLeadingZeros :: String -> String
+checkLeadingZeros "0" = "0" -- Special case for 0 allowed to start with 0
+checkLeadingZeros "-0" = "" -- Special case for -0 not allowed
+checkLeadingZeros ('0' : _) = ""
+checkLeadingZeros ('-' : xs) = '-' : checkLeadingZeros xs
+checkLeadingZeros s = s
 
 parse :: String -> Maybe BencodeValue
 parse ('i' : xs) = fmap fst (parseInt ('i' : xs))
@@ -26,11 +35,22 @@ parseInt ('i' : str) = fmap (first BencodeInt) (parseIntInternal str "")
       Nothing -> Nothing
     parseIntInternal (x : xs) acc = parseIntInternal xs (x : acc)
     parseIntInternal [] _ = Nothing
-
-    checkLeadingZeros :: String -> String
-    checkLeadingZeros "0" = "0" -- Special case for 0 allowed to start with 0
-    checkLeadingZeros "-0" = "" -- Special case for -0 not allowed
-    checkLeadingZeros ('0' : _) = ""
-    checkLeadingZeros ('-' : xs) = '-' : checkLeadingZeros xs
-    checkLeadingZeros s = s
 parseInt _ = Nothing
+
+parseStr :: String -> Maybe (BencodeValue, String)
+parseStr s = case parseLength s "" of
+  Just (n, body) ->
+    if length body < n
+      then
+        Nothing
+      else Just ((BencodeString . take n) body, drop n body)
+  Nothing -> Nothing
+
+parseLength :: String -> String -> Maybe (Int, String)
+parseLength (x : xs) acc
+  | x == ':' = case (readMaybe . checkLeadingZeros . reverse) acc of
+      Just n -> Just (n, xs)
+      Nothing -> Nothing
+  | isNumber x = parseLength xs (x : acc)
+  | otherwise = Nothing
+parseLength [] _ = Nothing
